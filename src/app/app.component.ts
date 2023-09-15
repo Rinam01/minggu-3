77437532a005c6414acca87c3f9bd82b07c81c91
Rinam01 <inamrinam72@gmail.com>
2023-09-15 11:19:04 +0700

    belum selesai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularBootstrap';
+  nama = 'Rinam';
+  nim = '212102430';
+  title = 'Angular Bootstrap';
 }
